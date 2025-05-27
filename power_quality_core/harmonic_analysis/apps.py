72715205b2501196f35afa0eeaffcb5bbef654eb
Yash Taank <22bee122@nith.ac.in>
2025-05-27 22:19:15 +0530

    First commit (the whole project stucture goes by this configuration)

---
+++ power_quality_core/harmonic_analysis/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HarmonicAnalysisConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'harmonic_analysis'
---
+++ power_quality_core/power_metrics/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PowerMetricsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'power_metrics'
